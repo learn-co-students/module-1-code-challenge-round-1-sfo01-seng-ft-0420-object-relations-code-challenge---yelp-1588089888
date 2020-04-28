02741e9f21a664ccc08fbb6e5d4967f9752a81ec
Tisdale511 <tisdalefry@gmail.com>
2020-04-28 12:01:09 -0700

    done

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,29 @@
 class Restaurant
   attr_reader :name
-
+  @@all = []
   def initialize(name)
     @name = name
+    @@all << self
+  end
+
+  def reviews
+    Review.all.select do |rev_obj|
+      self == rev_obj.restaurant
+    end
+  end
+
+  def customers
+    custs = Review.all.select do |revs|
+      self == revs.restaurant
+    end
+    custs.uniq
+    # custs.map do |cust|
+    #   cust.given_name
+    #   binding.pry
+  end
+
+  def self.all
+    @@all
   end
 
 end
--- tools/console.rb
+++ tools/console.rb
@@ -7,5 +7,18 @@ end
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
+customer1 = Customer.new("Tisdale", "Fry")
+customer2 = Customer.new("Tina", "Fey")
+customer3 = Customer.new("Rick", "Short")
+customer4 = Customer.new("Matt", "Smith")
+
+restaurant1 = Restaurant.new("McDonalds")
+restaurant2 = Restaurant.new("Arby's")
+
+review1 = Review.new(customer1, restaurant1, 4)
+review2 = Review.new(customer3, restaurant1, 8)
+review3 = Review.new(customer2, restaurant2, 6)
+review4 = Review.new(customer4, restaurant2, 7)
+
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
