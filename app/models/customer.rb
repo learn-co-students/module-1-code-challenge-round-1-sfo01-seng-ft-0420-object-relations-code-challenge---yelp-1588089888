1ae75ebf001f7f229922c596384644e80fc0ef69
finalbout <johntjr.22@gmail.com>
2020-04-28 11:02:43 -0700

    whatever

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,12 +1,60 @@
 class Customer
   attr_accessor :given_name, :family_name
 
+  @@all = []
+
+
   def initialize(given_name, family_name)
     @given_name = given_name
     @family_name  = family_name
+    
+    @@all << self
   end
 
   def full_name
     "#{given_name} #{family_name}"
   end
+
+  def self.all
+    @@all
+  end
+
+  def restaurants(name) #works
+    Restaurant.all.each {|n| n.name == name}
+  end
+
+  def add_review(restaurant, rating) #works
+    Review.new(self,restaurant,rating)
+  end
+
+
+  def num_reviews(name) #not working
+    Review.all.select {|n| n.rating == name}
+    #c1.num_reviews("John") => total of reviews of c1
+  end
+
+  def self.find_by_name(name) #works?
+    finder = Customer.all.select {|n| n.family_name == name}
+  end
+
+
+  def self.find_all_by_given_name(name) #works
+    Customer.all.select {|n|n.given_name == name}
+  end
+
+
+
 end
+
+
+
+c1 = Customer.new("John", "Tom")
+c2 = Customer.new("Frances", "Gonzales")
+c3 = Customer.new("Julia", "Tom")
+
+c4 = Customer.new("John", "Chan")
+
+c5 = Customer.new("John", "Tommmy")
+
+c6 = Customer.new("John", "Tong")
+
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,56 @@
 class Review
   
+
+
+    attr_accessor :rating
+    attr_reader :customer, :restaurant
+
+    @@all = [ ]
+
+
+    def initialize(customer, restaurant, rating)
+        @customer = customer
+        @restaurant = restaurant
+        @rating = rating
+
+
+
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def rating(restaurant)
+
+
+        # Review.all.map {|n| n.rating == restaurant}
+        
+        # rating << Restaurant.all.select {|r| r.restaurant == rating}
+
+        #instance.rating("Panda Express") => rating
+    end
+
+
+    def customer
+        
+
+        # Customer.all.select {|r| r.given_name == review}÷
+
+        #return review
+        #create a review, cant change customer
+        #r1.customer => 
+    end
+
+    def restaurant
+        
+    end
+
+
+
 end
+
+r1 = Review.new("John","Panda Express", 4)
+r2  = Review.new("Frances","In n Out", 3)
+r3 = Review.new("Julia","Chevys", 4)
