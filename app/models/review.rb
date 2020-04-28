682f03e308d143d271ee54df6d9bb75507430e84
Don Mallory <dmm333@gmail.com>
2020-04-28 09:48:31 -0700

    Nailed It?

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,18 @@
 class Review
+    attr_reader :rating, :customer, :restaurant
+    @@all = []
+
+    def initialize(restaurant, customer, rating)
+        @customer = customer
+        @restaurant = restaurant
+        @rating = rating
+        @@all << self
+    end
+
   
+
+    def self.all
+        @@all
+    end
+
 end
--- tools/console.rb
+++ tools/console.rb
@@ -3,9 +3,37 @@ require_relative '../config/environment.rb'
 def reload
   load 'config/environment.rb'
 end
-# Insert code here to run before hitting the binding.pry
-# This is a convenient place to define variables and/or set up new object instances,
-# so they will be available to test and play around with in your console
+
+
+
+del_popolo = Restaurant.new("Del Popolo")
+mister_jius = Restaurant.new("Mister Jiu's")
+coi = Restaurant.new("Coi")
+republique = Restaurant.new("Republique")
+rich_table = Restaurant.new("Rich Table")
+
+don = Customer.new("Don", "Mallory")
+steven = Customer.new("Steven", "Parsons")
+tisdale = Customer.new("Tisdale", "Fry")
+maggie = Customer.new("Magdalena", "Cooper")
+don_king = Customer.new("Don", "King")
+
+Review.new(del_popolo, don, 5)
+Review.new(del_popolo, don, 3) #bad second experience!?
+Review.new(del_popolo, maggie, 2) #didn't like the wine selection
+Review.new(del_popolo, tisdale, 5)
+
+Review.new(republique, don, 5)
+Review.new(republique, steven, 5)
+Review.new(republique, maggie, 5)  #apparently they crush it
+Review.new(republique, tisdale, 5)
+
+Review.new(coi, don, 4)
+
+
+
+
+
 
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
