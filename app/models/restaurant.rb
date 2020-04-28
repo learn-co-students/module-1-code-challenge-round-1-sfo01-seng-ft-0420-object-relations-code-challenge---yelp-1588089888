ae5bac1e096e7bd1dbd7979f1347ff835b38ac07
Don Mallory <dmm333@gmail.com>
2020-04-28 09:55:32 -0700

    Nailed it....

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -17,7 +17,7 @@ class Restaurant
 
   def average_star_rating
     ratings = reviews.map {|review| review.rating} #Store in local variable to prevent two Class Method calls when one will do
-    (ratings.sum / ratings.count.to_f).round(1) #1 decimal place sounds nice
+    (ratings.sum / ratings.count.to_f).round(1) #1 decimal place sounds nice here (3.8 rating etc.)
   end
 
   def self.all
