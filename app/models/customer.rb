b2e92b36520ff3feee3192989230f5744e71216e
tharveybrown <tharveybrown@gmail.com>
2020-04-28 09:45:36 -0700

    solution to mod1 challenge

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,12 +1,43 @@
 class Customer
   attr_accessor :given_name, :family_name
-
+  @@all = []
+  
   def initialize(given_name, family_name)
     @given_name = given_name
     @family_name  = family_name
+    @@all << self
   end
 
   def full_name
     "#{given_name} #{family_name}"
+  end
+  
+  def restaurants
+    my_reviews.map {|review| review.restaurant}.uniq
+  end
+
+  def add_review(restaurant, rating)
+    Review.new(self, restaurant, rating)
+  end
+
+  def num_reviews
+    my_reviews.size
+  end
+
+  def self.find_by_name(full_name)
+    @@all.find {|customer| customer.full_name == full_name}
+  end
+
+  def self.find_all_by_given_name(given_name)
+    @@all.find_all {|customer| customer.given_name == given_name}
+  end
+
+  def self.all 
+    @@all
+  end
+
+  private
+  def my_reviews
+    Review.all.select {|review| review.customer == self}
   end
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -5,4 +5,17 @@ class Restaurant
     @name = name
   end
 
+  def reviews
+    Review.all.select{|review| review.restaurant == self}
+  end
+
+  def customers
+    reviews.map {|review| review.customer}.uniq
+  end
+
+  def average_star_rating
+    ratings = reviews.map {|review| review.rating}
+    average = ratings.sum.to_f / ratings.size
+  end
+
 end
