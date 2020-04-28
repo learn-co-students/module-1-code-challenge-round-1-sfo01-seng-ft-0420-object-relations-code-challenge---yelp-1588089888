72533a9d636e19b392663396024a8fdebff97c91
Guosa_Edebiri <gedebiri@gmail.com>
2020-04-28 11:01:12 -0700

    I didn't test this code

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,12 +1,54 @@
 class Customer
   attr_accessor :given_name, :family_name
 
+  @@all = []
+
   def initialize(given_name, family_name)
     @given_name = given_name
     @family_name  = family_name
+    @@all << self
   end
 
   def full_name
     "#{given_name} #{family_name}"
   end
+
+  def given_name
+    @given_name
+  end
+
+  def family_name
+    @family_name
+  end
+
+  def self.all
+    @@all
+  end
+
+  def restaurants
+    Review.all.uniq do |x| x.restaurant.customer = self
+    end
+  end
+
+  def add_review(restaurant, rating)
+    new_review = Review.new
+    new_review.customer = self.customer
+    new_review.restaurant = restaurant
+  end
+
+  def num_reviews
+    Review.all.count do |x| x.customer == self 
+    end
+  end
+
+  def find_by_name(full_name_string)
+    @@all.find do |x| x.full_name == full_name_string 
+    end
+  end
+
+  def find_all_by_given_name(given_name_string)
+    @all.map do |x| x.given_name == given_name_string 
+    end
+  end
+
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -5,4 +5,25 @@ class Restaurant
     @name = name
   end
 
+  def name
+    @name
+  end
+
+  def reviews
+    Review.all.map do |x| x.restaurant == self 
+    end
+  end
+
+  def customers
+    Customer.all.uniq do |x| x.review.restaurant == self 
+    end
+  end
+
+  def average_star_rating
+    sum = Review.all.sum do |x| x.restaurant == self end
+    num = Review.all.count do |x| x.restaurant == self end
+    avg = sum / num
+    avg
+  end
+
 end
