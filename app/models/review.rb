28aa9241e988a9367de409a796c10ac74e87da71
aa1b02ab7492d696da6e8ac540ffa187ba623c88e15238d8d026f79bc5d39b3c <darrickpang@DESKTOP-02GI1R6.localdomain>
2020-04-28 11:05:02 -0700

    Submission

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,62 @@
 class Review
-  
+    @@all = []
+
+    def initialize(customer, restaurant, rating)
+        @customer = customer
+        @restaurant = restaurant
+        @rating = rating
+        @@all << self
+    end 
+
+    def rating
+        @rating 
+    end 
+
+    def review(restaurant_name)
+        arr = []
+        self.each do |elem|
+            if(elem.restaurant == restaurant_name)
+                arr.push(elem.rating)
+            end 
+        end 
+        return arr
+    end 
+
+    def customers(name)
+        arr = []
+        self.each do |names|
+            if(self.names == name)
+                arr.push(self.names)
+            end 
+        end 
+        return arr
+    end 
+
+    def num_reviews(name)
+        arr = []
+        self.each do |names|
+            if(self.names == name)
+                arr.push(self.names)
+            end 
+        end 
+        return arr.length
+    end 
+    def average_star_rating(restaurant_name)
+        arr = []
+        self.each do |elem|
+            if(elem.restaurant == restaurant_name)
+                arr.push(elem.rating)
+            end 
+        end 
+        sum = 0 
+        avg = 0
+        arr.each do |x|
+            sum += x.rating 
+            avg = sum/arr.length 
+        end 
+        return avg 
+    end 
+    def self.all 
+        @@all 
+    end 
 end
